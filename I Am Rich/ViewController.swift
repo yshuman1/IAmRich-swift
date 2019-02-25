2a94bc83e6becca445d7c9a3fbd64e660bb190e2
Yasin Shuman <email@yasin.io>
2019-02-25 03:59:11 -0800

    copy of famous I am Rich App from back in the day - complete

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by yasin shuman on 2/25/19.
+//  Copyright © 2019 yasin shuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
